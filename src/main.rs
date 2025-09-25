ff812a5e070c789863498466d6a9b1f2fb478031
Nathan Henrie <nate@n8henrie.com>
2025-09-24 18:49:17 -0600

    Try to reduce temp with `CpuClock::_80MHz`

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ esp_bootloader_esp_idf::esp_app_desc!();
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::_80MHz);
     let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(size: 72 * 1024);
@@ -102,11 +102,8 @@ async fn main(spawner: Spawner) {
         EspWifiController<'static>,
         esp_wifi::init(timg0.timer0, rng).expect("couldn't init esp_wifi")
     );
-    let (mut controller, interfaces) = esp_wifi::wifi::new(esp_wifi_ctrl, peripherals.WIFI)
+    let (controller, interfaces) = esp_wifi::wifi::new(esp_wifi_ctrl, peripherals.WIFI)
         .expect("couldn't create wifi controller");
-    controller
-        .set_power_saving(PowerSaveMode::Maximum)
-        .expect("couldn't set power save mode");
     let wifi_interface = interfaces.sta;
 
     let timg1 = TimerGroup::new(peripherals.TIMG1);
